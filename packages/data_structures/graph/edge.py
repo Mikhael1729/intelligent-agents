44ca2afd34d3e0b84a1ebbbb38c776aff80d8d7c
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-12-16 17:48:40 -0400

    Add types to Edge

--- packages/data_structures/graph/edge.py
+++ packages/data_structures/graph/edge.py
@@ -1,10 +1,16 @@
-class Edge:
-  """
-  value: Symbol of the transition
-  source: Node source
-  destination Node destination
-  """
-  def __init__(self, id, source, destination, value=None):
+from .node import Node
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
+
+class Edge(Generic[T]):
+  def __init__(
+    self,
+    id: int,
+    source: Node[T],
+    destination: Node[T],
+    value: T = None
+  ):
     self.id = id
     self.source = source
     self.destination = destination
