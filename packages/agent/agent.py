1a4332e2cb0cef3ab8c3bc93b6d1dfd8121300df
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-12-29 18:51:29 -0400

    Fix problem of large travel for the agent in the solution of the problem

--- packages/agent/agent.py
+++ packages/agent/agent.py
@@ -2,12 +2,17 @@ import math
 from ..data_structures.priority_queue import PriorityQueue 
 from ..data_structures.graph.graph import Graph, Edge
 from abc import abstractmethod, ABCMeta
-from typing import TypeVar, List, Generic, Callable, Generic
+from typing import TypeVar, List, Generic, Callable, Generic, Tuple, Dict
 from graphviz import Digraph as VizDigraph, Graph as VizGraph
 import numbers
 
 T = TypeVar("T")
 U = TypeVar("U")
+
+class Action:
+  def __init__(self, path: Edge[T, U], distance: int):
+    self.path = path
+    self.distance = distance
 
 """
 It can navigate intelligently through a states space graph.
@@ -15,22 +20,27 @@ It can navigate intelligently through a states space graph.
 class Agent(Generic[T], metaclass=ABCMeta):
   def __init__(self):
     self.__states_space: Graph[T] = self.create_states_space()
-    self.__actions: List[Edge[T, U]] = []
+    self.__actions: Dict[int, Action] = {}
+    self.__solution: Dict[int, Edge[T, U]] = {}
 
   @property
   def states_space(self) -> Graph[T]:
     return self.__states_space
 
   @property
-  def actions(self):
+  def actions(self) -> Dict[int, Action]:
     return self.__actions
+
+  @property
+  def solution(self) -> List[Edge[T, U]]:
+    return self.__solution
 
   def state_exists_gbfs(self, goal_state: T) -> (int, List[Edge[T, U]]):
     return self.__state_exists(goal_state, self.heuristic_function)
 
   def state_exists_asearch(self, goal_state: T) -> (int, List[Edge[T, U]]):
     evaluation_function: Callable[[Edge[T, U]], int] = lambda edge: (
-      self.heuristic_function(edge) + self.distance_function(edge, self.actions)
+      self.heuristic_function(edge) + self.distance_function(edge, self.actions[edge.id].distance)
     )
 
     return self.__state_exists(goal_state, evaluation_function)
@@ -39,8 +49,6 @@ class Agent(Generic[T], metaclass=ABCMeta):
     if len(self.__states_space.nodes) == 0:
       return False
 
-    self.__actions = []
-
     edge = Edge[T, U](
       id = -1,
       source = None,
@@ -48,37 +56,36 @@ class Agent(Generic[T], metaclass=ABCMeta):
       value = 0
     )
 
-    path: List[Edge[T, U]] = []
+    closed: List[Edge[T, U]] = []
     opened: PriorityQueue[T] = PriorityQueue[Edge[T, U]](
       init_elements=[edge],
       map_value=evaluation_function
     )
 
-    closed: List[Edge[T, U]] = []
-
+    self.actions[edge.id] = Action(edge, 0)
     while len(opened) != 0:
       most_promising_path: Edge[T, U] = opened.dequeue()
-      path.append(most_promising_path)
+      self.solution[most_promising_path.id] = most_promising_path
       closed.append(most_promising_path)
-
-      self.__actions.append(most_promising_path.id)
 
       goal_is_found = most_promising_path.destination.value == goal_state
       if goal_is_found:
         closed.append(most_promising_path)
-        return (True, path)
+        return True
 
       for edge in most_promising_path.destination.adjacents:
         if edge not in closed and edge not in opened:
           opened.enqueue(edge)
+          new_distance = self.actions[most_promising_path.id].distance + 1
+          self.actions[edge.id] = Action(edge, new_distance)
 
-    return (False, path)
+    return False
 
   @abstractmethod
   def heuristic_function(self, edge: Edge[T, U]) -> int:
     raise NotImplementedError
 
-  def distance_function(self, edge: Edge[T, U], actions: List[Edge[T, U]]) -> int:
+  def distance_function(self, edge: Edge[T, U], distance: int) -> int:
     return 0
 
   @abstractmethod
@@ -87,21 +94,24 @@ class Agent(Generic[T], metaclass=ABCMeta):
 
   def print_states_space(self) -> None:
     g = VizDigraph('G', filename='./test_output/states_space.gv')
-
     states_space: Graph[T]= self.states_space
 
     edge: Edge[T, U]
+    last_key = list(self.solution.keys())[-1]
     for i, edge in enumerate(states_space.edges):
       source_value = str(edge.source.value) 
       destination_value = str(edge.destination.value)
       label = str(edge.value)
 
       color = None
-      if edge.id in self.__actions:
-        if self.__actions[-1] == edge.id:
+      if edge.id in self.actions:
+        if edge.id == last_key:
           color = 'red'
         else: 
-          color = 'blue'
+          if edge.id in self.solution.keys():
+            color = 'green'
+          else:
+            color = 'blue'
 
       g.edge(source_value, destination_value, label=label, color=color )
 
