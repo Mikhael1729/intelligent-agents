89f905ce4743a7ddbc5b9d8c5794d4707699ce5f
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-12-28 21:13:09 -0400

    Crete an alternative of permute_iterably to allow to map the permuted values

--- permutations.py
+++ permutations.py
@@ -1,5 +1,7 @@
-from typing import List, TypeVar
+from typing import List, TypeVar, Tuple, Callable
+
 T = TypeVar('T')
+Transform = Callable[[List, int, int], any]
 
 def _permute_generetably(elements: List[T], i: int):
   """
@@ -32,13 +34,42 @@ def _permute_iterably(elements: List[T], permutations: List[T], i: int):
 
     _permute_iterably(elements, permutations, i - 1)
 
-def permutations(elements):
+def _permute_iterably2(
+    elements: List,
+    permutations: List,
+    i: int,
+    actions: Tuple[int, int],
+    transform: Transform = None
+):
+  if i == 1:
+    new_permutation = elements.copy()
+    if transform:
+      new_permutation = transform(new_permutation, actions[0], actions[1])
+
+    permutations.append(new_permutation)
+    return
+
+  _permute_iterably2(elements, permutations, i - 1, actions, transform)
+
+  for j in range(i - 1):
+    k = 0 if i % 2 == 1 else j
+    elements[k], elements[i - 1] = elements[i - 1], elements[k]
+
+    actions[0] = k
+    actions[1] = i - 1
+
+    _permute_iterably2(elements, permutations, i - 1, actions, transform)
+
+def permutations(elements, transform: Transform = None):
   """
   Compute all distinct permutations of elements list using the
   Heap's algorithm.
   """
   permutations = []
-  _permute_iterably(elements, permutations, len(elements))
+  size = len(elements)
+  actions = [-1, -1]
+
+  _permute_iterably2(elements, permutations, size, actions, transform)
   return permutations
 
 def generate_permutations(elements):
