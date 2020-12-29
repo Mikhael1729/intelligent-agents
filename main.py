01b71e963a4fef1834d868e4d8ed4536ddd9a772
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-12-28 21:17:19 -0400

    Test the fixed method (see previous commit) using state_exists_asearch

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from letters_agent import LettersAgent
 from colors_agent import ColorsAgent
 
 agent = ColorsAgent()
-exists = agent.state_exists_asearch(['R', 'B', 'G', 'Y', 'R'])
+exists = agent.state_exists_asearch(['R', 'B', 'G', 'Y', 'R°'])
 
-agent.print_states_space()
-
-
-
+# Print the path in console.
 print("exists: ", exists[0])
 print("path: ")
 
 for path in exists[1]:
-	print("  - ", path.destination)
+ print("  - ", path.destination.value)
+
+# Show an image of the generated graph and the path taken.
+agent.print_states_space()
 
