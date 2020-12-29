e2f45a629f8f69a14fbba1a8ad063323e0d984af
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-12-29 18:52:13 -0400

    Implement the new version of the agent

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ agent = ColorsAgent()
 exists = agent.state_exists_asearch(['R', 'B', 'G', 'Y', 'R'])
 
 # Print the path in console.
-print("exists: ", exists[0])
+print("exists: ", exists)
 print("path: ")
 
-for path in exists[1]:
- print("  - ", path.destination.value)
+for key, value in agent.solution.items():
+  print(value)
 
 # Show an image of the generated graph and the path taken.
 agent.print_states_space()
+
+"""
+agent = LettersAgent()
+agent.state_exists_asearch('E')
+
+agent.print_states_space()
+"""
